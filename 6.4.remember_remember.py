47176d7237795586740ba338073fa6184083ea1d
David Cohen <deiv464@gmail.com>
2023-05-22 16:28:03 +0300

    add documention

--- 6.4.remember_remember.py
+++ 6.4.remember_remember.py
@@ -2,11 +2,39 @@ import cv2
 import numpy as np
 
 
-def remember(path):
-    img = cv2.imread(path, 0)
-    mid_color = int(np.median(img) * 0.8)
-    return ''.join(chr(row) for col in range(img.shape[1]) for row in range(img.shape[0]) if img[row][col] < mid_color)
+def remember(path: str) -> str:
+    """
+    Retrieves text from an image file.
+
+    The function reads an image file, performs processing, and extracts text from the image based on a threshold color value.
+    The resulting text is returned as a string.
+
+    Args:
+        path (str): The path to the image file.
+
+    Returns:
+        str: The extracted text from the image.
+
+    Raises:
+        IOError: If the image file cannot be read or does not exist.
+
+    Examples:
+        >>> remember('image.jpg')
+        'ExtractedText123'
+
+    """
+    image = cv2.imread(path, 0)
+
+    if image is None:
+        raise IOError("Unable to read the image file or file does not exist.")
+
+    mid_color = int(np.median(image) * 0.8)
+    extracted_text = ''.join(
+        chr(row) for col in range(image.shape[1]) for row in range(image.shape[0]) if image[row][col] < mid_color)
+
+    return extracted_text
 
 
 if __name__ == '__main__':
-    print(remember("C:/Users/user/Downloads/code.png"))
+    print(remember("code.png"))
+    #Place gunpowder beneath the House of Lords. 11/05/1605
