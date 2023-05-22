e27859c14e3e8acf089ec8a4704fd7f282e29927
David Cohen <deiv464@gmail.com>
2023-05-22 17:51:07 +0300

    change accordingly

--- 6.5.group_by.py
+++ 6.5.group_by.py
@@ -1,13 +1,15 @@
-def group_by(func, L):
-    d = {}
-    for i in L:
-        x = func(i)
-        if x in d:
-            d[x].append(i)
-        else:
-            d[x] = [i]
-    return d
+def group_by1(func, list_objects):
+    result = {}
+    {result.setdefault(func(obj), []).append(obj) for obj in list_objects}
+    return result
+
+
+def group_by2(func, list_objects):
+    return {func(obj): list(filter(lambda value: func(value) == func(obj), list_objects)) for obj in list_objects}
 
 
 if __name__ == '__main__':
-    print(group_by(len, ["hi", "bye", "yo", "try"]))
+    print(group_by1(sum, [[1, 2, 3], [3, 3], [4, 4, 4], [8, 4], [6], [111]]))
+    print(group_by2(sum, [[1, 2, 3], [3, 3], [4, 4, 4], [8, 4], [6], [111]]))
+    # {6: [[1, 2, 3], [3, 3], [6]], 12: [[4, 4, 4], [8, 4]], 111: [[111]]}
+
